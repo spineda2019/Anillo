55403e5f412e04887cd3939821466d63da81c970
spineda2019 <spineda.wpi.alum@gmail.com>
2026-04-20 18:15:17 -0400

    stub arg parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use crate::token::{Ast, ExternalFunctionNode, Ingot, IsrNode, Token};
+use crate::token::{Ast, ExternalFunctionNode, FuncArg, Ingot, IsrNode, Token};
 
 #[derive(Debug)]
 pub struct Parser {
@@ -36,11 +36,30 @@ impl Parser {
         if let Some(Token::Identifier(func_name)) = self.tokens.pop_front()
             && let Some(Token::LeftParen) = self.tokens.pop_front()
         {
+            // We're lucky we have chained let binding now. IIRC this was unstable for a bit
+
+            let args = self.parse_func_args();
+            if let Some(&Token::KeywordWithLevel) = self.tokens.front() {
+            } else {
+                return ExternalFunctionNode {
+                    name: func_name,
+                    args,
+                    privilege: None,
+                };
+            }
+            match self.tokens.front() {
+                Some(&Token::KeywordWithLevel) => {}
+                _ => {}
+            }
             dbg!(&func_name, &self.tokens);
             todo!();
         } else {
             panic!("");
         }
+    }
+
+    fn parse_func_args(&mut self) -> Vec<FuncArg> {
+        todo!()
     }
 
     fn parse_isr(&mut self) -> IsrNode {
