db94779777651a89bb76bef23bf25736c04f181d
spineda2019 <spineda.wpi.alum@gmail.com>
2026-04-26 13:54:36 -0400

    priv and arg validation

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@ pub struct CompilationError {
     line: u32,
     column: u32,
     diagnostic: String,
+    src_available: bool,
 }
 
 impl CompilationError {
@@ -10,6 +11,16 @@ impl CompilationError {
             line,
             column,
             diagnostic,
+            src_available: true,
+        }
+    }
+
+    pub fn new_without_src_info(diagnostic: String) -> CompilationError {
+        CompilationError {
+            line: 0,
+            column: 0,
+            diagnostic,
+            src_available: false,
         }
     }
 }
@@ -18,8 +29,15 @@ impl std::fmt::Debug for CompilationError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "CompilationError: {{line = {}, column = {}, diagnostic = {}}}",
-            self.line, self.column, self.diagnostic
+            "CompilationError: {{line = {}, column = {}, diagnostic = {}}}{}",
+            self.line,
+            self.column,
+            self.diagnostic,
+            if self.src_available {
+                ""
+            } else {
+                "\n(Source info unavaliable)"
+            }
         )
     }
 }
