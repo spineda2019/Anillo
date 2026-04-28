43e4ba1746e23f6556837f14edf210c9d512534e
Fluxx7 <nwa31104@gmail.com>
2026-04-28 19:37:50 -0400

    updated docs to reflect new argument

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,8 @@ use crate::compilerc::CompilerC;
 ///   Enables in-progress printing of the AST as it is being built as
 ///   well as other useful information as the compiler runs (such as
 ///   the initial token buffer produced by the lexer)
+/// * **_--comp <filename>_**
+///   \<UNFINISHED> Produces C header and source files containing a function _AnilloISRRegister()_ which initializes and registers the described IDT from the Anillo source
 /// * **_-h_** or **_--help_**
 ///   Prints command line help
 fn main() -> Result<(), Box<dyn Error>> {
